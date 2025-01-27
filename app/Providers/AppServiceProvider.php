e881204727f907340e41529a014d06e4d200c26d
Clinton Deacon <clinton.deacon.2016@gmail.com>
2025-01-27 17:16:21 +0000

    Implement rate limiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\ShortenedUrl;
 use App\Observers\ShortenedUrlObserver;
 use App\Services\UrlShorteningService;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +18,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(UrlShorteningService::class, function ($app) {
             return new UrlShorteningService();
+        });
+
+        RateLimiter::for('api', function ($request) {
+            return Limit::perSecond(10)->by($request->ip());
         });
     }
 
